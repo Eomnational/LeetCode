321a4e125da7cae53beacdd74e192c41f92d2dbb
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-05-14 00:51:09 +0800

    feat: 198.打家劫舍-5.14

---
+++ 26.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    let dp=new Array(nums.length);
+    dp[0]=nums[0],dp[1]=Math.max(nums[0],nums[1]);
+    for(let i=2;i<nums.length;i++){
+        dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
+    }
+
+    return dp[nums.length-1];
+};
+
+
+// 示例 1：
+
+// 输入：[1,2,3,1]
+// 输出：4
+// 解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+//      偷窃到的最高金额 = 1 + 3 = 4 。
+// 示例 2：
+
+// 输入：[2,7,9,3,1]
+// 输出：12
+// 解释：偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+//      偷窃到的最高金额 = 2 + 9 + 1 = 12 。
