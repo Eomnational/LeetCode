f9d0a01e4bce04125cd305796f3cce6f0a930c19
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-05-15 23:32:56 +0800

    feat: 139.单词拆分-5.15

---
+++ 29.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+const wordBreak = (s, wordDict) => {
+
+    let dp = Array(s.length + 1).fill(false);
+    dp[0] = true;
+
+    for(let i = 0; i <= s.length; i++){
+        for(let j = 0; j < wordDict.length; j++) {
+            if(i >= wordDict[j].length) {
+                if(s.slice(i - wordDict[j].length, i) === wordDict[j] && dp[i - wordDict[j].length]) {
+                    dp[i] = true
+                }
+            }
+        }
+    }
+
+    return dp[s.length];
+}
+
+
+// 示例 1：
+
+// 输入: s = "leetcode", wordDict = ["leet", "code"]
+// 输出: true
+// 解释: 返回 true 因为 "leetcode" 可以由 "leet" 和 "code" 拼接成。
+// 示例 2：
+
+// 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+// 输出: true
+// 解释: 返回 true 因为 "applepenapple" 可以由 "apple" "pen" "apple" 拼接成。
+//      注意，你可以重复使用字典中的单词。
