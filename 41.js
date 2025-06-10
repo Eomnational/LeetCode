d273cfb2087100cb0b5241cf89d535a30a22d64c
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-06-11 00:40:13 +0800

    feat: 55.跳跃游戏-6.11

---
+++ 41.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let n=nums.length;
+    let target=nums.length-1;
+    for(let i=n-2;i>=0;i--){
+        if(nums[i]+i>=target){
+            target=i;
+        }
+    }
+    if(target===0)return true;
+    return false;
+};
