74e9d9e9373b7e12c68d0cc3d80f3d5afd88cb6c
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-06-19 11:17:26 +0800

    feat: 102.二叉树的层序遍历-6.19

---
+++ 54.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+  let res=[],queue=[];
+  queue.push(root);
+  if(root===null){
+    return res;
+  };
+  while(queue.length!==0){
+    let cur=[];
+    let len=queue.length;
+    for(let i=0;i<len;i++){
+        let node=queue.shift();
+        cur.push(node.val);
+        node.left&&queue.push(node.left);
+        node.right&&queue.push(node.right);
+    }
+
+    res.push(cur);
+  }
+  return res;
+  
+
+
+};
