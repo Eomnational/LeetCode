052b026ccea0076002062e3acdecc37e9b051c8d
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-08-27 11:08:35 +0800

    feat: 128.最长连续序列-8.27

---
+++ 复习-8.27/3.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    nums.sort((a,b)=>a-b);
+    let set=new Set(nums);
+    let max=0
+    for(let temp of set){
+        if(set.has(temp-1)===false){
+            let start=temp;
+            while(set.has(temp)){
+                temp++;
+            } 
+            let end=temp;
+            max=Math.max(max,temp-start);  
+        }
+    }
+    return max;
+};
