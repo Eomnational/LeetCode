3731fe4713c3116defdd20ea394a5be601206cee
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-08-27 10:48:26 +0800

    feat: 49.字母异位词分组-8.27

---
+++ 复习-8.27/2.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let map=new Map();
+    for(let i=0;i<strs.length;i++){
+        let c=strs[i].split('').sort().join('');
+        if(map.has(c)){
+            map.get(c).push(strs[i]);
+        }else{
+            map.set(c,[strs[i]]);
+        }
+    }
+    return Array.from(map.values());
+};
