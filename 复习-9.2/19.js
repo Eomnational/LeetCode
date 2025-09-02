e30590683bd8fb529d0c4ff0abc0403401409ef3
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-09-02 23:24:13 +0800

    feat: 141.环形链表-9.2

---
+++ 复习-9.2/19.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let t=head,p=head;
+    while(p!=null&&p.next!=null){
+        t=t.next;
+        p=p.next.next;
+        if(t===p){
+            return true;
+        }
+    }
+
+    return false;
+};
