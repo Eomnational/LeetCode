4df504373df10025445fab5688bb2826e64b5612
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-09-02 21:43:31 +0800

    feat: 238.移动零-9.2

---
+++ 复习-9.2/4.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let k=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]!=0){
+            nums[k]=nums[i];
+            k++;
+        }
+    }
+
+    for(let j=0;j<nums.length;j++){
+        if(j>=k){
+            nums[j]=0;
+        }
+    }
+    return nums;
+};
