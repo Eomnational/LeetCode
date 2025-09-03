ee00530a3e64ee4cd5a4d8a8d7df368be81eeeb1
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-09-03 17:15:09 +0800

    feat: 35.搜索插入位置-9.3

---
+++ 复习-9.3/1.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let l=0,r=nums.length-1;
+    while(l<=r){
+        let mid=l+((r-l)>>1);
+        if(nums[mid]>target){
+            r=mid-1;
+        }else if(nums[mid]<target){
+            l=mid+1;
+        }else{
+            return mid;
+        }
+    }
+
+    return l;
+};
