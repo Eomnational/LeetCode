5e2c3c5efeeb640bd32275098bf8f886e5d13f99
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-09-03 17:35:41 +0800

    feat: 240.搜索二维矩阵II-9.3

---
+++ 复习-9.3/3.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+     for(let i=0;i<matrix.length;i++){
+        let left=0,right=matrix[0].length-1;
+        while(left<=right){
+            let mid=left+((right-left)>>1);
+            if(matrix[i][mid]>target){
+                right=mid-1;
+            }else if(matrix[i][mid]<target){
+                left=mid+1;
+            }else{
+                return true;
+            }
+        }
+     }
+     return false;
+};
