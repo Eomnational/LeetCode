7f25e82c9f22cc81cb0952082b48be8d87422369
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-09-05 01:55:46 +0800

    feat: 79.单词搜索-9.4

---
+++ 复习-9.4/6.js
@@ -0,0 +1,33 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+    let m=board.length,n=board[0].length;
+    let used=Array.from({length:m},()=>new Array(n).fill(false) );
+
+    const dfs=(i,j,k)=>{
+        if(k==word.length){
+            return true;
+        }
+
+        if(i<0||i>=m||j<0||j>=n||board[i][j]!==word[k]||used[i][j]){
+            return false;
+        }
+
+        used[i][j]=true;
+        const res=dfs(i-1,j,k+1)||dfs(i+1,j,k+1)||dfs(i,j-1,k+1)||dfs(i,j+1,k+1);
+        used[i][j]=false;
+        return res;
+    }
+
+    for(let i=0;i<m;i++){
+        for(let j=0;j<n;j++){
+            if(dfs(i,j,0)){
+                return true;
+            }
+        }
+    }
+    return false;
+};
