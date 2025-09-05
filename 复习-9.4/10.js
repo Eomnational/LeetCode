3eee017201915ac72faada38e80a6a4dea8f13cd
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-09-05 08:57:08 +0800

    feat: 169.多数元素-9.4

---
+++ 复习-9.4/10.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let res=0,count=0;
+    for(let i=0;i<nums.length;i++){
+
+        if(count==0){
+            res=nums[i];
+        }
+        if(res===nums[i]){
+            count++;
+        }else{
+            count--;
+        }
+    }
+    return res;
+};
