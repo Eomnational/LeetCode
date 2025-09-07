bf9d4f91a4ad65570c9bab65382090ee1b63b440
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-09-07 20:18:35 +0800

    feat: 105.从前序与中序遍历序列构造二叉树-9.6

---
+++ 复习-9.6/10.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+    if(!preorder.length){
+        return null;
+    }
+    let rootval=preorder.shift();
+    let index=inorder.indexOf(rootval);
+    let root=new TreeNode(rootval);
+    root.left=buildTree(preorder.slice(0,index),inorder.slice(0,index));
+    root.right=buildTree(preorder.slice(index),inorder.slice(index+1));
+
+    return root;
+};
