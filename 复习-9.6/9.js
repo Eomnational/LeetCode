988c690371ef82600bd7f642a7d8a24a3d79417c
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-09-07 20:05:30 +0800

    feat: 114.二叉树展开为链表-9.6

---
+++ 复习-9.6/9.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root) {
+    while(root){
+        if(!root.left){
+            root=root.right;
+        }else{
+        let temp=root.left;
+        while(temp.right){
+            temp=temp.right;
+        }
+        temp.right=root.right;
+        root.right=root.left;
+        root.left=null;
+
+        }
+    }
+    return root;
+};
