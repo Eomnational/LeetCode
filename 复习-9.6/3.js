f69ba3333be759e8ca1f74e0d92f1134158216ba
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-09-07 17:09:19 +0800

    feat: 543.二叉树的直径-9.6

---
+++ 复习-9.6/3.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
+    let max=0;
+
+    const test=(root)=>{
+        if(root==null){
+            return 0;
+        }
+
+        let maxleft=test(root.left);
+        let maxright=test(root.right);
+        max=Math.max(max,maxleft+maxright);
+        return Math.max(maxleft,maxright)+1;
+    }
+
+    test(root);
+    return max;
+};
