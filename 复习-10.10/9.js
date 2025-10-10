eca20d2c0d648a663b4eb6e8353f85dd3f543338
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-10-10 14:56:38 +0800

    feat: 54.螺旋矩阵-10.10

---
+++ 复习-10.10/9.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+    let res=[];
+    let l=0,u=0,r=matrix[0].length-1,d=matrix.length-1;
+    while(true){
+
+        //上边界
+        for(let i=l;i<=r;i++){
+            res.push(matrix[u][i]);
+        }
+        u++;
+        if(u>d){
+            break;
+        }
+
+        //右
+        for(let i=u;i<=d;i++){
+            res.push(matrix[i][r]);
+        }
+        r--;
+        if(l>r){
+            break;
+        }
+
+        //下
+        for(let i=r;i>=l;i--){
+            res.push(matrix[d][i]);
+        }
+        d--;
+        if(u>d){
+            break;
+        }
+
+        //左
+        for(let i=d;i>=u;i--){
+            res.push(matrix[i][l]);
+        }
+        l++;
+        if(l>r){
+            break;
+        }
+    }
+    return res;
+};
