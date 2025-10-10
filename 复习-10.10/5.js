02385def3011fb8b3697a94ba38e412be0f88d3a
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-10-10 12:04:06 +0800

    feat: 11.盛最多水的容器-10.10

---
+++ 复习-10.10/5.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left=0,right=height.length-1;
+    let area=0;
+    while(left<right){
+        let h=Math.min(height[left],height[right]);
+        area=Math.max(area,h*Math.abs(right-left));
+        height[left]<height[right]?left++:right--;
+    }
+    return area;
+};
