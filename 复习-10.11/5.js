e6a9f95a27ec1eb7f9ded802cc0bbb7dc685b1f7
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-10-11 23:22:01 +0800

    feat: 78.子集-10.11

---
+++ 复习-10.11/5.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    let res=[],path=[];
+
+    const dfs=(start)=>{
+        res.push([...path]);
+        if(start>nums.length){
+            return;
+        }
+
+        for(let i=start;i<nums.length;i++){
+            path.push(nums[i]);
+            dfs(i+1);
+            path.pop();
+        }
+    }
+
+    dfs(0);
+    return res;
+};
