2117b34348582e01d1936dc3bb2471e5d6835dfe
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-10-22 00:35:04 +0800

    feat: 189.轮转数组-10.21

---
+++ 复习-10.21/12.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    
+    const swap=(nums,l,r)=>{
+        while(l<r){
+        [nums[l],nums[r]]=[nums[r],nums[l]];
+        l++;
+        r--;
+        }
+    }
+
+    let n=nums.length;
+    k=k%n;
+    swap(nums,0,n-1);
+    swap(nums,0,k-1);
+    swap(nums,k,n-1);
+
+    return nums;
+};
