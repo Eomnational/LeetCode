2ffa54bcd1e394c2c4834348990bdf81b6509ed2
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-10-21 20:37:42 +0800

    feat: 560.和为 K 的子数组-10.21

---
+++ 复习-10.21/9.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    let sum=0;
+    let ans=0;
+    let map=new Map();
+    map.set(0,1);
+    for(let i=0;i<nums.length;i++){
+        sum+=nums[i];
+
+        if(map.has(sum-k)){
+            ans+=map.get(sum-k);
+        }
+
+        map.set(sum,(map.get(sum)||0)+1);
+    }  
+
+    return ans;
+};
