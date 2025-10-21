11baf007033a32d8739e0c026d4bddcb79eebb4a
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-10-22 01:00:29 +0800

    feat: 73.矩阵置零-10.21

---
+++ 复习-10.21/14.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+    let map1=new Map();
+    let map2=new Map();
+
+    for(let i=0;i<matrix.length;i++){
+
+        for(let j=0;j<matrix[0].length;j++){
+            if(matrix[i][j]==0){
+            map1[i]=i;
+            map2[j]=j;
+            }
+        }
+    }
+
+    for(let i in map1){
+        for(let j=0;j<matrix[0].length;j++){
+            matrix[i][j]=0;
+        }
+    }
+
+    for(let j in map2){
+        for(let i=0;i<matrix.length;i++){
+            matrix[i][j]=0;
+        }
+    }
+
+    return matrix;
+};
