cdc9f6ed3617fe61e8b83d87cbbc39aad0a28ad2
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-10-22 00:53:49 +0800

    feat: 238.除自身以外数组的乘积-10.21

---
+++ 复习-10.21/13.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let res=[];
+    res[0]=1;
+    let right=1;
+
+    for(let i=1;i<nums.length;i++){
+        res[i]=res[i-1]*nums[i-1];
+    }
+
+    for(let j=nums.length-1;j>=0;j--){
+        res[j]=res[j]*right;
+        right=right*nums[j];
+    }
+
+    return res;
+};
