a13cd75a814fee068cae33ade247359beb0cf89c
Zero1017 <109895777+Eomnational@users.noreply.github.com>
2025-10-22 18:12:56 +0800

    feat: 20.有效的括号-10.22

---
+++ 复习-10.22/15.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let res=[];
+
+    for(let i=0;i<s.length;i++){
+        let c=s[i];
+        switch(c){
+            case '(':
+                res.push(')');
+                break;
+            case '{':
+                res.push('}');
+                break;
+            case '[':
+                res.push(']');
+                break;
+            default:
+            if(res.pop()!==c){
+                return false;
+            }
+        }
+    }
+
+    return res.length==0;
+};
